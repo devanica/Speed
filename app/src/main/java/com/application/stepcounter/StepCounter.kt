ea7e3695ecd17f5e053a182326bd014778d5e12a
Anica <developer.anica@gmail.com>
2020-06-26 16:55:09 +0200

    Fixed multiple steps in a second issue.

--- app/src/main/java/com/application/stepcounter/StepCounter.kt
+++ app/src/main/java/com/application/stepcounter/StepCounter.kt
@@ -3,26 +3,27 @@ package com.application.stepcounter
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
+import android.util.Log
 import java.sql.Timestamp
+import kotlin.time.ExperimentalTime
+import kotlin.time.milliseconds
 import kotlin.time.seconds
 
 class StepCounter(private val mStepListener: StepListener) : SensorEventListener {
 
     var currentTime = Timestamp(System.currentTimeMillis())
     var newTime = Timestamp(System.currentTimeMillis())
-    var currentStep = 0
 
+    @ExperimentalTime
     override fun onSensorChanged(event: SensorEvent) {
-        if (event.values[0] > 20 || event.values[1] > 20 || event.values[2] > 20) {
+        if (event.values[1] >= 10) {
+            Log.e("x axis: ", event.values[0].toString())
+            Log.e("y axis: ", event.values[1].toString())
+            Log.e("z axis: ", event.values[2].toString())
             newTime = Timestamp(System.currentTimeMillis())
-            if (currentTime.seconds != newTime.seconds){
-                if (currentStep == 1){
-                    currentStep = 0
-                    return
-                }
+            if (currentTime.time.seconds != newTime.time.seconds){
                 currentTime = Timestamp(System.currentTimeMillis())
                 mStepListener.onStepTaken()
-                currentStep = 1
             }
         }
     }
