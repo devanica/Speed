cd566cb5d4d086516dbda11f9e6903f30a03b94e
Anica <developer.anica@gmail.com>
2020-07-03 18:56:53 +0200

    Added branded screen

--- app/src/main/java/com/application/stepcounter/BrandingScreen.kt
+++ app/src/main/java/com/application/stepcounter/BrandingScreen.kt
@@ -7,6 +7,7 @@ import android.view.animation.LinearInterpolator
 import android.view.animation.RotateAnimation
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import kotlinx.coroutines.*
 
 class BrandingScreen : AppCompatActivity() {
@@ -30,7 +31,6 @@ class BrandingScreen : AppCompatActivity() {
 
                 leftSneaker.clearAnimation()
                 rightSneaker.clearAnimation()
-                onDestroy()
             }
         }
     }
